8744175d184219635a0184836129d23780d95b9f
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-07 16:32:23 +0900

    feat: 단어장 엔티티 설정 및 회원 엔티티 인덱스 타입 수정

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/entity/Member.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/entity/Member.java
@@ -14,6 +14,7 @@ public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT UNSIGNED")
     private Long id;
 
     @Column(nullable = false)
