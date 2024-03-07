7b08399e4be244da3d59bfd94cbf15159ad77acd
송강산 <rkdtks0816@naver.com>
2024-03-07 08:12:02 +0000

    feat : mysql 외부 포트 열기

--- Install/install_mysql.sh
+++ Install/install_mysql.sh
@@ -16,3 +16,13 @@ sudo systemctl enable mysql
 
 # MySQL 루트 비밀번호 설정
 sudo mysql_secure_installation
+
+# MySQL 설정 변경
+sudo cp ./mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+
+# 추가 쿼리 실행
+sudo mysql -u root -p -e "create user 'root'@'%' identified by 'tttt';"
+sudo mysql -u root -p -e "grant all privileges on *.* to 'root'@'%' with grant option;"
+
+# MySQL 재시작
+sudo service mysql restart
