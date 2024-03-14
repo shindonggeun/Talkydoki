81af3830bb670daa4fec451870cce3b7112dfb16
이지호 <jeho1129@naver.com>
2024-03-14 18:01:03 +0900

    feat: WordCount 코드 수정

--- BackEnd/FastApiServer/main.py
+++ BackEnd/FastApiServer/main.py
@@ -1,14 +1,25 @@
 # main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
 from sqlalchemy import MetaData
-from database import engine
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine, Base
 from models.vocabulary import Vocabulary
+from models.news import News
 from routes.vocabulary import router as vocabulary_router
-
+import subprocess
 
 app = FastAPI()
 
 app.include_router(vocabulary_router)
+
+# 데이터베이스 세션 생성
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 # 서버 시작 시 테이블 생성
 # @app.on_event("startup")
@@ -25,3 +36,36 @@ app.include_router(vocabulary_router)
 #     metadata = MetaData()
 #     # 지정된 테이블만 삭제
 #     metadata.drop_all(engine, tables=[Vocabulary.__table__])
+
+Base.metadata.create_all(bind = engine)
+
+def get_news(db: Session):
+    return db.query(News.id, News.title, News.content, News.summary).all()
+
+def save_data(news_data, filename = "news_data.txt"):
+    with open(filename, "w", encoding = "utf-8") as file:
+        for news in news_data:
+            file.write(f"ID : {news.id}\nTITLE : {news.title}\nSUMMARY : {news.summary}\nCONTENT : {news.content}\n")
+
+@app.get("/fetch-news")
+def fetch_news_to_file(db: Session = Depends(get_db)):
+    news_data = get_news(db)
+    save_data(news_data)
+    return {"message": "News data fetched and saved successfully!"}
+
+@app.post("/test/")
+async def test_hadoop_streaming():
+    # mapper.py 실행 후 출력을 파일에 직접 저장
+    mapper_cmd = "python wordcount_mapper.py < news_data.txt > temp_mapper_output.txt"
+    subprocess.run(mapper_cmd, shell=True, check=True)
+
+    # reducer.py 실행
+    reducer_cmd = "sort temp_mapper_output.txt | python wordcount_reducer.py"
+    reducer_result = subprocess.run(reducer_cmd, shell=True, text=True, capture_output=True)
+
+    # 임시 파일 삭제
+    subprocess.run("rm temp_mapper_output.txt", shell=True)
+
+    # reducer의 결과를 반환
+    processed_data = reducer_result.stdout
+    return {"processed_data": processed_data}
