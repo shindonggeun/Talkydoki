fb22e014d453c66eb534535cb90f14332e9c9e6c
이지호 <jeho1129@naver.com>
2024-03-18 14:44:22 +0900

    feat: 크롤링 후 요청 시 자동 Hadoop 실행 + 자동 데이터 추출 및 저장 구현(EC2, Local 환경 나눠서 구현)

--- BackEnd/FastApiServer/main.py
+++ BackEnd/FastApiServer/main.py
@@ -4,14 +4,17 @@ from fastapi.responses import FileResponse
 from sqlalchemy import MetaData
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine, Base
+from datetime import datetime
 from models.vocabulary import Vocabulary
 from models.news import News
 from routes.vocabulary import router as vocabulary_router
-import requests
+import requests, subprocess, os
 
 app = FastAPI()
 
 API_URL = "http://j10c107.p.ssafy.io:8080"
+
+execution_env = os.getenv("EXECUTION_ENV", "LOCAL")
 
 app.include_router(vocabulary_router)
 
@@ -44,26 +47,58 @@ Base.metadata.create_all(bind = engine)
 def get_news(db: Session):
     return db.query(News.id, News.title, News.content, News.summary).all()
 
-def save_data(news_data, filename = "news_data.txt"):
-    with open(filename, "w", encoding = "utf-8") as file:
+def save_data(news_data, filename = "news_data.txt", path="/input"):
+    full_path = f"{path}/{filename}"
+    with open(full_path, "w", encoding = "utf-8") as file:
         for news in news_data:
             file.write(f"ID\n{news.id}\nTITLE\n{news.title}\nSUMMARY\n{news.summary}\nCONTENT\n{news.content}\n")
 
+def generate_output_path(base_path="/TF_output"):
+    timestamp = datetime.now().strftime("%Y%m%d")
+    return f"{base_path}/{timestamp}"
+
+def start_hadoop_streaming():
+    output_path = generate_output_path()
+    hadoop_command = f"hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-*.jar -files /home/ubuntu/data-processing/TF_mapper.py,/home/ubuntu/data-processing/TF_reducer.py -mapper 'python3 /home/ubuntu/data-processing/TF_mapper.py' -reducer 'python3 /home/ubuntu/data-processing/TF_reducer.py' -input /input/news_data.txt -output /TF_output/{output_path}"
+    try:
+        subprocess.run(hadoop_command, check=True, shell=True)
+        return True
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing Hadoop Streaming Job : {e}")
+        return False
+
 @app.get("/fetch-news")
-def fetch_news_to_file(db: Session = Depends(get_db)):
+async def fetch_news_to_file(db: Session = Depends(get_db)):
     news_data = get_news(db)
-    save_data(news_data)
-    return {"message": "News data fetched and saved successfully!"}
+    
+    if execution_env == "EC2":
+        save_data(news_data, path="/input")
+        if start_hadoop_streaming():
+            return {"message": "News data fetched and Hadoop Streaming job started successfully!"}
+        else:
+            return {"message": "News data fetched, but failed to start Hadoop Streaming job."}
+    else:
+        save_data(news_data)
+        return {"message": "News data fetched and saved successfully!"}
 
 @app.get("/extract-japanese")
 async def extract_japanese():
-    input_filename = "local_part-00000.txt"
-    try:
-        with open(input_filename, "r", encoding = "utf-8") as file:
-            content = file.readlines()
-    except FileNotFoundError:
-        raise HTTPException(status_code = 404, detail = "File not found")
+    if execution_env == "EC2":
+        base_path = "/TF_output"
+        today_str = datetime.now().strftime("%Y%m%d")
+        input_path = os.path.join(base_path, today_str, "part-00000")
+        
+        if not os.path.exists(input_path):
+            raise HTTPException(status_code=404, detail="Result file not found for today.")
+    else:
+        input_filename = "local_part-00000.txt"
 
+        try:
+            with open(input_filename, "r", encoding = "utf-8") as file:
+                content = file.readlines()
+        except FileNotFoundError:
+            raise HTTPException(status_code = 404, detail = "File not found")
+    
     unique_words = set()
 
     for line in content:
@@ -82,6 +117,10 @@ async def extract_japanese():
 @app.get("/upload-keywords")
 async def upload_keywords():
     input_filename = "japanese.txt"
+    if execution_env == "EC2":
+        if not os.path.exists(input_filename):
+            raise HTTPException(status_code=404, detail="japanese.txt file not found")
+
     try:
         with open(input_filename, "r", encoding="utf-8") as file:
             for line in file:
