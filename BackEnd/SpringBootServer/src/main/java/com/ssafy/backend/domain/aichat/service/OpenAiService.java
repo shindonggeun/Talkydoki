07cec22f864314bfb69775f6ef995ddb248dc486
조현호 <chh5680@naver.com>
2024-03-19 20:05:03 +0900

    refactor : 평가 response 타입 및 ResponseEntity로 return하도록 수정 중

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/OpenAiService.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/OpenAiService.java
@@ -1,7 +1,9 @@
 package com.ssafy.backend.domain.aichat.service;
 
 import com.ssafy.backend.domain.aichat.dto.AiChatMessage;
+import com.ssafy.backend.domain.aichat.dto.api.AiChatConversation;
 import com.ssafy.backend.domain.aichat.dto.api.AiChatReportCreateRequest;
+import com.ssafy.backend.domain.aichat.dto.api.AiChatReportCreateResponse;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
@@ -9,5 +11,10 @@ import java.util.Map;
 public interface OpenAiService {
     public Mono<String> sendMessage(AiChatMessage createRequest);
 
-    public Mono<Map<String, Object>> createReport(AiChatReportCreateRequest aiChatReportCreateRequest);
+    public Mono<Map<String, Object>> createReport(Long roomId, AiChatConversation aiChatConversation);
+
+//    public Mono<AiChatReportCreateRequest> createReport(AiChatConversation aiChatConversation);
+
+//    public Mono<AiChatReportCreateResponse> saveReport(Long roomId, AiChatConversation aiChatConversation);
+
 }
