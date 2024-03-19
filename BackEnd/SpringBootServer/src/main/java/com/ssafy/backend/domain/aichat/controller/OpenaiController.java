dfdf6f357617e5b31db2b1c97f807c53cf6a6e18
조현호 <chh5680@naver.com>
2024-03-19 14:33:43 +0900

    feat : report 분석 최소 기능 구현 Json 형식으로 ai_chat_report의 항목들을 포함하는 객체로 응답 가능

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/OpenaiController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/OpenaiController.java
@@ -1,20 +1,30 @@
 package com.ssafy.backend.domain.aichat.controller;
 
 import com.ssafy.backend.domain.aichat.dto.AiChatMessage;
+import com.ssafy.backend.domain.aichat.dto.api.AiChatReportCreateRequest;
 import com.ssafy.backend.domain.aichat.service.OpenAiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class OpenaiController {
     private final OpenAiService openAiService;
 
-    @PostMapping("/api/v1/gpt")
+    @PostMapping("/gpt")
     public Mono<String> sendMessage(@RequestBody AiChatMessage message){
         return openAiService.sendMessage(message);
+    }
+
+    @PostMapping("/gpt/report")
+    public Mono<Map<String,Object>> createReport(@RequestBody AiChatReportCreateRequest request) {
+        return openAiService.createReport(request);
     }
 }
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/OpenAiService.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/OpenAiService.java
@@ -1,9 +1,13 @@
 package com.ssafy.backend.domain.aichat.service;
 
 import com.ssafy.backend.domain.aichat.dto.AiChatMessage;
-import com.ssafy.backend.domain.aichat.dto.api.OpenAiMessage;
+import com.ssafy.backend.domain.aichat.dto.api.AiChatReportCreateRequest;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 public interface OpenAiService {
     public Mono<String> sendMessage(AiChatMessage createRequest);
+
+    public Mono<Map<String, Object>> createReport(AiChatReportCreateRequest aiChatReportCreateRequest);
 }
