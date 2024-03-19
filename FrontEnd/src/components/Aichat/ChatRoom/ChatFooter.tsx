78105ddd17d5b662246272e0d759184255f1fb92
오기선 <pakox123@gmail.com>
2024-03-19 17:15:22 +0900

    feat: ai 채팅 stt 추가

--- FrontEnd/src/components/Aichat/ChatRoom/ChatFooter.tsx
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatFooter.tsx
@@ -1,18 +1,60 @@
 import { FooterContainer } from "@/styles/Aichat/AiChatRoom";
-import MicIcon from "@mui/icons-material/Mic";
 import { useState } from "react";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import MicIcon from "@mui/icons-material/Mic";
+
+// // 스피치 api 임포트
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
 type Props = {};
 
 // 녹음 하는중 시각화 추가 필요
 function ChatFooter({}: Props) {
-  const [isRecording, setIsRecording] = useState(false);
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [message, setMessage] = useState("");
+  // 타이머
+  const [timer, setTimer] = useState<number | undefined>(undefined);
+
+  // 스피치 api
+  const { transcript, browserSupportsSpeechRecognition, resetTranscript } =
+    useSpeechRecognition();
+
   const toggleRecording = () => {
+    if (isRecording) {
+      setMessage(transcript);
+
+      SpeechRecognition.stopListening();
+      resetTranscript();
+      clearTimeout(timer);
+    } else {
+      setMessage("");
+
+      SpeechRecognition.startListening({ continuous: true, language: "ja-JP" });
+      // 17초 타이머 설정
+
+      const newTimer = setTimeout(() => {
+        SpeechRecognition.stopListening();
+        resetTranscript();
+      }, 17000);
+      setTimer(newTimer);
+    }
     setIsRecording(!isRecording);
   };
+
   const cancelRecording = () => {
     setIsRecording(false);
+    SpeechRecognition.stopListening();
+    resetTranscript();
+    SpeechRecognition.abortListening();
   };
+
+  // 크롬 사용하지 않은 경우 출력 메세지(예외처리)
+  if (!browserSupportsSpeechRecognition) {
+    return <span>크롬 브라우저를 사용해주세요</span>;
+  }
+
   return (
     <>
       <FooterContainer>
@@ -27,6 +69,10 @@ function ChatFooter({}: Props) {
             />
           </div>
         )}
+        {/* api 연결 후 삭제예정 */}
+        <div>변환음성{transcript}</div>
+
+        <div>저장 메세지{message}</div>
         <div
           className={`micdiv ${isRecording ? "recording" : ""}`}
           onClick={toggleRecording}
