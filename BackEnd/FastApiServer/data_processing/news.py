d236b8110b874559f79349b2af5c7c8bf92e26a6
이지호 <jeho1129@naver.com>
2024-03-20 12:08:07 +0900

    fix: EC2 내 Hadoop 실행 로직 수정

--- BackEnd/FastApiServer/data_processing/news.py
+++ BackEnd/FastApiServer/data_processing/news.py
@@ -24,24 +24,31 @@ def get_db():
 def get_news(db: Session):
     return db.query(News.id, News.title, News.content, News.summary).all()
 
-def save_data_ec2(news_data, filename = "news_data.txt", path="/input"):
-    full_path = f"{path}/{filename}"
-    with open(full_path, "w", encoding = "utf-8") as file:
+def save_data(news_data, base_path="/input"):
+    today_str = datetime.now().strftime("%Y%m%d")
+    filename = f"news_data_{today_str}.txt"
+    full_path = os.path.join(base_path, filename)
+    with open(full_path, "w", encoding="utf-8") as file:
         for news in news_data:
             file.write(f"ID\n{news.id}\nTITLE\n{news.title}\nSUMMARY\n{news.summary}\nCONTENT\n{news.content}\n")
+    return full_path
 
-def save_data_local(news_data, filename = "news_data.txt"):
-    with open(filename, "w", encoding = "utf-8") as file:
-        for news in news_data:
-            file.write(f"ID\n{news.id}\nTITLE\n{news.title}\nSUMMARY\n{news.summary}\nCONTENT\n{news.content}\n")
+def copy_to_hdfs(local_path, hdfs_path="/input"):
+    hadoop_fs_put_command = f"hdfs dfs -put {local_path} {hdfs_path}"
+    try:
+        subprocess.run(hadoop_fs_put_command, check=True, shell=True)
+        print(f"File {local_path} copied to HDFS path {hdfs_path} successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error copying file to HDFS: {e}")
+        raise
 
-def generate_output_path(base_path="/TF_output"):
+def generate_output_path(base_path="/output"):
     timestamp = datetime.now().strftime("%Y%m%d")
     return f"{base_path}/{timestamp}"
 
-def start_hadoop_streaming():
+def start_hadoop_streaming(input_file):
     output_path = generate_output_path()
-    hadoop_command = f"hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-*.jar -files /home/ubuntu/data-processing/TF_mapper.py,/home/ubuntu/data-processing/TF_reducer.py -mapper 'python3 /home/ubuntu/data-processing/TF_mapper.py' -reducer 'python3 /home/ubuntu/data-processing/TF_reducer.py' -input /input/news_data.txt -output /TF_output/{output_path}"
+    hadoop_command = f"hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-*.jar -files /home/ubuntu/data-processing/TF_mapper.py,/home/ubuntu/data-processing/TF_reducer.py -mapper 'python3 /home/ubuntu/data-processing/TF_mapper.py' -reducer 'python3 /home/ubuntu/data-processing/TF_reducer.py' -input {input_file} -output {output_path}"
     try:
         subprocess.run(hadoop_command, check=True, shell=True)
         return True
@@ -52,41 +59,33 @@ def start_hadoop_streaming():
 @router.get("/fetch-news")
 async def fetch_news_to_file(db: Session = Depends(get_db)):
     news_data = get_news(db)
-    
-    if execution_env == "EC2":
-        save_data_ec2(news_data, path="/input")
-        if start_hadoop_streaming():
-            message = "News data fetched and Hadoop Streaming job started successfully!"
-            # Hadoop Streaming 작업 성공 시 extract_japanese 호출
-            await extract_japanese()
-        else:
-            message = "News data fetched, but failed to start Hadoop Streaming job."
+    input_file = save_data(news_data)
+    copy_to_hdfs(input_file)
+    hdfs_input_file = "/input/" + os.path.basename(input_file)
+    if start_hadoop_streaming(hdfs_input_file):
+        message = "News data fetched and copied to HDFS successfully. Hadoop Streaming job started successfully!"
+        await extract_japanese()
     else:
-        save_data_local(news_data)
-        message = "News data fetched and saved successfully!"
+        message = "News data fetched and copied to HDFS, but failed to start Hadoop Streaming job."
     
     return {"message": message}
 
 @router.get("/extract-japanese")
 async def extract_japanese():
-    if execution_env == "EC2":
-        base_path = "/TF_output"
-        today_str = datetime.now().strftime("%Y%m%d")
-        input_path = os.path.join(base_path, today_str, "part-00000")
-        
-        if not os.path.exists(input_path):
-            raise HTTPException(status_code=404, detail="Result file not found for today.")
-    else:
-        input_filename = "local_part-00000.txt"
+    base_path = "output"
+    today_str = datetime.now().strftime("%Y%m%d")
+    input_path = os.path.join(base_path, today_str, "part-00000")
+    
+    if not os.path.exists(input_path):
+        raise HTTPException(status_code=404, detail="Result file not found for today.")
 
-        try:
-            with open(input_filename, "r", encoding = "utf-8") as file:
-                content = file.readlines()
-        except FileNotFoundError:
-            raise HTTPException(status_code = 404, detail = "File not found")
+    try:
+        with open(input_path, "r", encoding="utf-8") as file:
+            content = file.readlines()
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="File not found")
     
     unique_words = set()
-
     for line in content:
         words = line.split()
         if len(words) > 2:
@@ -94,21 +93,19 @@ async def extract_japanese():
             unique_words.add(japanese_word)
     
     result_filename = "japanese.txt"
-    with open(result_filename, "w", encoding = "utf-8") as file:
+    with open(result_filename, "w", encoding="utf-8") as file:
         for word in sorted(unique_words):
             file.write(word + "\n")
     
-    # extract_japanese 작업 성공 시 upload_keywords 호출
     await upload_keywords()
-
     return FileResponse(result_filename)
+
 
 @router.get("/upload-keywords")
 async def upload_keywords():
     input_filename = "japanese.txt"
-    if execution_env == "EC2":
-        if not os.path.exists(input_filename):
-            raise HTTPException(status_code=404, detail="japanese.txt file not found")
+    if not os.path.exists(input_filename):
+        raise HTTPException(status_code=404, detail="japanese.txt file not found")
 
     failed_keywords = []  # 실패한 키워드를 추적하기 위한 리스트
     try:
