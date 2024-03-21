40aa3d02056dceec2ad7ef5151b9f5e1de1d3ba0
오기선 <pakox123@gmail.com>
2024-03-22 08:54:08 +0900

    feat: 리스트 라우터 추가

--- FrontEnd/src/components/Aichat/AiChatCategory.tsx
+++ FrontEnd/src/components/Aichat/AiChatCategory.tsx
@@ -1,22 +1,27 @@
 import { Categorybox, NegativeTiTle } from "@/styles/Aichat/AiChatList";
 import { AiChatCard } from "@/styles/common/ui/card";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
 
 function AiChatCategory({}: Props) {
+  const navigate = useNavigate();
+
+  const goAiChatRoom = () => {
+    navigate("/aichatroom");
+  };
   return (
     <>
       <div className="titleBox">
         <NegativeTiTle>회화 리포트</NegativeTiTle>
       </div>
       <Categorybox>
-        <AiChatCard></AiChatCard>
-        <AiChatCard></AiChatCard>
-        <AiChatCard></AiChatCard>
-        <AiChatCard></AiChatCard>
-        <AiChatCard></AiChatCard>
-        <AiChatCard></AiChatCard>
-        <AiChatCard></AiChatCard>
+        <AiChatCard onClick={goAiChatRoom}></AiChatCard>
+        <AiChatCard onClick={goAiChatRoom}></AiChatCard>
+        <AiChatCard onClick={goAiChatRoom}></AiChatCard>
+        <AiChatCard onClick={goAiChatRoom}></AiChatCard>
+        <AiChatCard onClick={goAiChatRoom}></AiChatCard>
+        <AiChatCard onClick={goAiChatRoom}></AiChatCard>
       </Categorybox>
     </>
   );
