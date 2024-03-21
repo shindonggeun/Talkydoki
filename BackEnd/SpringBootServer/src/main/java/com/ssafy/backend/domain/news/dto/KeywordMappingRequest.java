347ec67ec3547d7f58f68b4011f07d9fd03376f7
이지호 <jeho1129@naver.com>
2024-03-21 14:33:47 +0900

    fix: SpringBoot 내 뉴스 - 단어 데이터 처리 API 구현

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/dto/KeywordMappingRequest.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/dto/KeywordMappingRequest.java
@@ -12,12 +12,9 @@ import lombok.Setter;
 @AllArgsConstructor
 public class KeywordMappingRequest {
 
-    @NotBlank(message = "뉴스 아이디는 필수 입력값입니다.")
     private Long newsId;
 
-    @NotBlank(message = "일본어는 필수 입력값입니다.")
     private String japanese;
 
-    @NotBlank(message = "가중치는 필수 입력값입니다.")
     private Float weight;
 }
