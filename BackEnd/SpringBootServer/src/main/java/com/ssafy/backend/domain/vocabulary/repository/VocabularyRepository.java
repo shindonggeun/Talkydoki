36b725e0bb8329ac3ba22dcdb9b17d7a1d50f2aa
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-29 17:14:56 +0900

    fix: 해당 단어 검색하기 쿼리 네이티브 쿼리로 변경 (해당 단어 중복될 경우 하나만 가져오게끔 수정)

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/repository/VocabularyRepository.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/repository/VocabularyRepository.java
@@ -12,6 +12,7 @@ public interface VocabularyRepository extends JpaRepository<Vocabulary, Long> {
     @Query(value = "SELECT * FROM vocabulary ORDER BY RAND() LIMIT 1", nativeQuery = true)
     Optional<Vocabulary> findRandom();
 
-    // 단어사전에서 일본어 단어로 검색하기
+    // 단어사전에서 일본어 단어로 검색하되, 가장 높은 ID를 가진 항목만 반환
+    @Query(value = "SELECT * FROM vocabulary WHERE japanese = ?1 ORDER BY id DESC LIMIT 1", nativeQuery = true)
     Optional<Vocabulary> findByJapanese(String japanese);
 }
