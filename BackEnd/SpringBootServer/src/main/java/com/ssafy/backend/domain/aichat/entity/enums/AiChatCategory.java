b72452e075c6aea20ca5990d0d60f611bcfc132e
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-31 23:30:24 +0900

    feat: AI 회화 채팅 카테고리 추가

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/entity/enums/AiChatCategory.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/entity/enums/AiChatCategory.java
@@ -7,15 +7,15 @@ import lombok.Getter;
  */
 @Getter
 public enum AiChatCategory {
-    SOCIETY("사회"),
-    WEATHER_AND_DISASTER("날씨 및 재난"),
-    POLITICS("정치"),
-    BUSINESS("비즈니스"),
-    GLOBAL("세계"),
-    SPORTS("스포츠"),
-    LIFE("생활"),
-    HAMBURGER_ORDER("햄버거 주문"),
-    FRIEND_WITH_CONVERSATION("친구와의 대화");
+    CHANGE_AT_CONVENIENCE_STORE("편의점에서 잔돈 받기"),
+    SOCCER_CONVERSATION("축구 대화하기"),
+    ORDER_HAMBURGER("햄버거 주문하기"),
+    HAIRCUT_AT_SALON("미용실에서 머리 자르기"),
+    DESCRIBE_HEALTH_CONDITION_AT_HOSPITAL("병원에서 몸상태 말하기"),
+    FIND_LOST_ITEM_AT_POLICE_STATION("경찰서에서 분실물 찾기"),
+    ENGAGE_IN_SOCIAL_DISCUSSION("사회적 토론하기"),
+    BEFRIEND_A_COLLEAGUE("동료와 친해지기"),
+    BRUNCH_CONVERSATION("브런치 식사 대화하기");
 
     private final String koreanName;
 
