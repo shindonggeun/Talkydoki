a786d1bb45dace7bb0ea156c3e01cd36f43f4ac5
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-01 00:39:44 +0900

    chore: 사용하지 않는 코드 정리

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/dto/AiChatReportCreateResponse.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/dto/AiChatReportCreateResponse.java
@@ -1,12 +1,6 @@
 package com.ssafy.backend.domain.aichat.dto;
 
-import lombok.Builder;
-
-import java.util.List;
-
-@Builder
 public record AiChatReportCreateResponse(
-        AiChatReportDetailInfo aiChatReportDetailInfo,
-        List<AiChatAndFeedbackInfo> aiChatAndFeedbackInfos
+        Long reportId
 ) {
 }
