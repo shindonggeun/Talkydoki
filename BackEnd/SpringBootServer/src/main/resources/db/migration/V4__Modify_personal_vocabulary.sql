f872c30cb1c0aa9c3a73246900e70fc9e538218d
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-04 07:38:59 +0900

    fix: 마이그레이션 다시 수정
    Former-commit-id: 27403ee62229335a3d042fa16cd3cd200e11682a

--- BackEnd/SpringBootServer/src/main/resources/db/migration/V4__Modify_personal_vocabulary.sql
+++ BackEnd/SpringBootServer/src/main/resources/db/migration/V4__Modify_personal_vocabulary.sql
@@ -1,4 +1,5 @@
 ALTER TABLE `personal_vocabulary`
 DROP FOREIGN KEY `FK_personal_vocabulary_member_id`,
-ADD CONSTRAINT `FK_personal_vocabulary_member_id_cascade` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE;
+     ADD KEY `FK_personal_vocabulary_member_id_new` (`member_id`),
+    ADD CONSTRAINT `FK_personal_vocabulary_member_id_new` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE;
 
