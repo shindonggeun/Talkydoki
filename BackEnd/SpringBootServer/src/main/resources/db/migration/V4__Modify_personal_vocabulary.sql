6d8e6c2ab1a2cd8d1211f95288e8da2dd9f3dc16
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-04 07:42:47 +0900

    fix: 다시 수정
    Former-commit-id: a12135900c92c90290f0bfeac9fbded62b0f2d4b

--- BackEnd/SpringBootServer/src/main/resources/db/migration/V4__Modify_personal_vocabulary.sql
+++ BackEnd/SpringBootServer/src/main/resources/db/migration/V4__Modify_personal_vocabulary.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `personal_vocabulary`
-DROP FOREIGN KEY `FK_personal_vocabulary_member_id`,
+DROP FOREIGN KEY `FK_personal_vocabulary_member_id_cascade`,
      ADD KEY `FK_personal_vocabulary_member_id_new` (`member_id`),
     ADD CONSTRAINT `FK_personal_vocabulary_member_id_new` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE;
 
