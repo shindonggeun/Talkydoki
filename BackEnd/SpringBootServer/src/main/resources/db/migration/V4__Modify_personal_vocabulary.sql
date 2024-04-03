d08bb239e32e5689a24bfefb60306f41d954cc88
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-04 07:45:08 +0900

    fix: 다시 원복
    Former-commit-id: d6c41c67c7b7d927b11a9d984ae1c6e3c7b7d015

--- BackEnd/SpringBootServer/src/main/resources/db/migration/V4__Modify_personal_vocabulary.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `personal_vocabulary`
-DROP FOREIGN KEY `FK_personal_vocabulary_member_id_cascade`,
-     ADD KEY `FK_personal_vocabulary_member_id_new` (`member_id`),
-    ADD CONSTRAINT `FK_personal_vocabulary_member_id_new` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE;
-
